995c1e16146cfeb7d70debd0dc0ff7148354d11d
Robotshell <robotshelld@gmail.com>
2021-05-24 06:37:00 -0400

    Added main function, banner and help section

--- orhound.py
+++ orhound.py
@@ -15,7 +15,12 @@
 # You should have received a copy of the GNU General Public License
 # along with Knock. If not, see <http://www.gnu.org/licenses/>.
 
+# Standard Python libraries
 import sys
+import requests, re
+import random
+import time
+from bs4 import BeautifulSoup
 
 class colors:
     HEADER = '\033[95m'
@@ -36,9 +41,28 @@ def banner():
 | | | | |_) | |_| |/ _ \| | | | '_ \ / _` |
 | |_| |  _ <|  _  | (_) | |_| | | | | (_| |
  \___/|_| \_\_| |_|\___/ \__,_|_| |_|\__,_|
-
 """ + colors.ENDC)
 	print(colors.OKGREEN + "ORHound v.1.0 - Open Source Project\n" + "Author: Robotshell\n" + "Github: https://github.com/robotshell\n" + colors.ENDC)
+
+#CORE FUNCTION
+def dork(domain):
+#https://www.google.com/search?q=inurl%3A%3D%253Dhttp+site%3Atest.com
+	print (colors.OKCYAN + "Start Google dork scraping to find Open Redirects to " + colors.FAIL + domain + colors.ENDC)
+	with open("user_agent.txt") as ua:
+                random_user_agents = ua.readlines()
+	url = 'http://www.google.com/search?q=inurl%3A%3D%253Dhttp+site%3A'+domain
+	 
+	my_headers = { 'User-agent' : random.choice(random_user_agents).strip()}
+	r = requests.get( url, headers = my_headers)
+	print(r.text)
+	soup = BeautifulSoup( r.text, 'html.parser')
+	h3tags = soup.find_all( 'h3', class_='r')
+
+	for h3 in h3tags:
+		try:
+			print(re.search('url\?q=(.+?)\&sa',h3.a['href']).group(1))
+		except:
+			continue
 
 #MAIN FUNCTION
 def main():
@@ -53,11 +77,8 @@ def main():
 		print ("-v,--version" + colors.OKCYAN + "\tShow version" + colors.ENDC)
 		print ("-o,--output" + colors.OKCYAN + "\tEnable save output in .txt file" + colors.ENDC)
 	elif arg == "-v" or arg == "--version":
-		print ("a")
+		print ("ORHound v.1.0")
 	else:
-		print ("Adios")
+		dork(arg)
 	
-
-
-
 main()
